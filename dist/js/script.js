9afda504816ebcf348216b9a88efc5dedad5abac
Giorgi <g.omiadze@gmail.com>
2024-12-26 10:38:28 +0400

    Start footer

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,3 @@
 $(document).ready(function () {
-  // Start intlTelInput
-  function phoneMask() {
-    $('.phone_mask').intlTelInput({
-      utilsScript: "js/utils.js",
-      preferredCountries: ["ru", "by", "ua", "kz"]
-    });
-  }
 
-  phoneMask();
 });
