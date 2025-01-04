516fe8177245d3ae90f77cbf57140bd832db4360
Giorgi <g.omiadze@gmail.com>
2025-01-04 21:43:08 +0400

    End day 04,01,2025

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
   $('[data-fancybox]').fancybox({
     autoFocus: false,
   });
+
+  $('.headerBtn').on('click', function () {
+    $(this).toggleClass('active');
+    $('.headerMenu').slideToggle();
+    $('.headerRightMenu').toggleClass('active');
+  });
 });
