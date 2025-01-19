62826a3f4133c5148763d9c8c054d827ff2e1b65
Giorgi <g.omiadze@gmail.com>
2025-01-19 22:42:52 +0400

    End day 19,01,2024

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,4 +8,89 @@ $(document).ready(function () {
     $('.headerMenu').slideToggle();
     $('.headerRightMenu').toggleClass('active');
   });
+
+
+  const swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  const itemSlider = new Swiper(".itemSlider", {
+    cssMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+
+
+  $('.itemTabs .cnt_1').css("visibility", "visible");
+
+  $('.itemTabs .tab_1').click(function () {
+    $('.itemTabs .cnt_1').css("visibility", "visible");
+    $('.itemTabs .cnt_2, .itemTabs .cnt_3').css("visibility", "hidden");
+    $('.itemTabs .tab_1').addClass('active');
+    $('.itemTabs .tab_2, .itemTabs .tab_3').removeClass('active');
+  });
+
+
+  $('.itemTabs .tab_2').click(function () {
+    $('.itemTabs .cnt_1, .itemTabs .cnt_3').css("visibility", "hidden");
+    $('.itemTabs .cnt_2').css("visibility", "visible");
+    $('.itemTabs .tab_2').addClass('active');
+    $('.itemTabs .tab_1, .itemTabs .tab_3').removeClass('active');
+  });
+
+  $('.itemTabs .tab_3').click(function () {
+    $('.itemTabs .cnt_1, .itemTabs .cnt_2').css("visibility", "hidden");
+    $('.itemTabs .cnt_3').css("visibility", "visible");
+    $('.itemTabs .tab_3').addClass('active');
+    $('.itemTabs .tab_1, .itemTabs .tab_2').removeClass('active');
+  });
+
+
+//   Start slider
+
+});
+
+$(document).ready(function () {
+  const swiper = new Swiper(".liveCompanySlider", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination-bullets",
+      type: "bullets",
+      clickable: true,
+    },
+    on: {
+      init: function () {
+        updateFractionPagination(this);
+      },
+      slideChange: function () {
+        updateFractionPagination(this);
+      },
+    },
+  });
+
+
+  function updateFractionPagination(swiper) {
+    const currentSlide = swiper.realIndex + 1;
+    const totalSlides = swiper.slides.length - 2;
+    const fractionContainer = document.querySelector(".swiper-pagination-fraction");
+    fractionContainer.textContent = `${currentSlide} / ${totalSlides}`;
+  }
 });
